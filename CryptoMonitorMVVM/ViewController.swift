c84c6641fe269d3e5f8e7baeabb066aeb4f1a1bd
Em1rio <48679388+Em1rio@users.noreply.github.com>
2023-10-18 16:23:03 +0300

    Initial Commit

---
+++ CryptoMonitorMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoMonitorMVVM
+//
+//  Created by Emir Nasyrov on 18.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
