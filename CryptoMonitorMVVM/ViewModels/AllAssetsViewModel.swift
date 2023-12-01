645bdc318d92f6563983d0da46d1d00c8520284e
Em1rio <48679388+Em1rio@users.noreply.github.com>
2023-12-01 19:58:22 +0300

    Настроил отображение общего баланса всех активов

--- CryptoMonitorMVVM/ViewModels/AllAssetsViewModel.swift
+++ CryptoMonitorMVVM/ViewModels/AllAssetsViewModel.swift
@@ -12,8 +12,10 @@ final class AllAssetsViewModel {
     // MARK: - Variables
     private let networkManager: NetworkManagerProtocol
     private let dataBaseManager: DBManagerProtocol
+    private var totalCost: Decimal128 = 0.0
     private(set) var coinsAsCategory: Results<CoinCategory>?
     var callTableView: (()-> Void)?
+    var callBalanceLabel: ((_ totalCost: String) -> Void)?
 
     // MARK: - Lifecycle
     init(_ networkManager: NetworkManagerProtocol,
@@ -25,25 +27,68 @@ final class AllAssetsViewModel {
     func numberOfRow(_ section: Int) -> Int {
         if let coins = coinsAsCategory, !coins.isEmpty  {
             return coins.count
-        } else {
-            return 0
-        }
-        
+        } else { return 0}
     }
     func loadDataFromDatabase()  {
-        coinsAsCategory = dataBaseManager.getCoinsAsCategory()
+        coinsAsCategory = dataBaseManager.getObject(ofType: CoinCategory.self)
         callTableView?()
     }
-    let allAssetsModel = ["ETH", "BTC"]
+
+    //MARK: - Update All Assets Cost Label
+    func fetchDataFromDB() {
+        totalCost = 0.0
+        guard let coins = coinsAsCategory?.toArray(ofType: CoinCategory.self) as? [CoinCategory] else {return}
+        for item in coins {
+            fetchData(for: item)
+        }
+    }
     
-//    func fetchTickerDetails(withID id: String) {
-//        networkManager.fetchTickerDetails(withID: id) { result in
-//            switch result {
-//            case .success(let data):
-//                // Обработка данных о деталях тикера
-//            case .failure(let error):
-//                // Обработка ошибки
-//            }
-//        }
-//    }
+   private func fetchData(for item: CoinCategory) {
+        let idNumber = item._id
+        networkManager.fetchTickerDetails(withID: idNumber) { result in
+        switch result {
+                case .success(let coinData):
+                    do {
+                        let coin = try JSONDecoder().decode([OneCoin].self, from: coinData)
+                        self.coinsData(coin, for: item)
+                    } catch {
+                        print("Failed to decode coin data with error: \(error)")
+                    }
+                case .failure(let error):
+                    print("Network request failed with error: \(error)")
+                }
+            }
+    }
+    
+   private func coinsData(_ coin: [OneCoin] , for item: CoinCategory) {
+       let priceRightNow = try! Decimal128(string: coin.first?.priceUsd ?? "0")
+       DispatchQueue.main.async { [weak self] in
+        guard let self = self, let coinID = coin.first?.id else { return }
+           let realmQuery = self.dataBaseManager.getRealmQuery(
+               forType: CoinCategory.self,
+               where: "_id",
+               equals: coinID)
+           
+                if let quantity = realmQuery.first?.coinQuantity {
+                   let rawTotalCost = self.calculateTotalCost(priceRightNow, quantity)
+                   self.totalCost += rawTotalCost
+                   let formattedTotalCost = Formatter.shared.formatCurrency(inputValue: "\(self.totalCost)")
+                   self.updateUI(with: formattedTotalCost)
+               } else {
+                   return
+               }
+           }
+        
+    }
+    private func calculateTotalCost(_ price: Decimal128, _ quantity: Decimal128) -> Decimal128 {
+        return price * quantity
+    }
+    private func updateUI(with totalCost: String) {
+            self.callBalanceLabel?(totalCost)
+    }
 }
+//MARK: - TODO: Список задач
+// Обновление UI делать не в цикле
+// Посчитать разницу в стоимости портфеля за все время
+// Сделать возможность отсортировать в алфавитном порядке или по стоимости активов
+
