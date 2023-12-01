191c16175e04255e7886a9edb028e2d3833b3138
Em1rio <48679388+Em1rio@users.noreply.github.com>
2023-12-01 19:54:26 +0300

    Создал расширние для структуры Results в RealmSwift

--- CryptoMonitorMVVM/Extensions/Results.swift
+++ CryptoMonitorMVVM/Extensions/Results.swift
@@ -5,4 +5,16 @@
 //  Created by Emir Nasyrov on 01.12.2023.
 //
 
-import Foundation
+import RealmSwift
+
+extension Results {
+    func toArray<T>(ofType: T.Type) -> [T] {
+        var array = [T]()
+        for i in 0 ..< count {
+            if let result = self[i] as? T {
+                array.append(result)
+            }
+        }
+        return array
+    }
+}
