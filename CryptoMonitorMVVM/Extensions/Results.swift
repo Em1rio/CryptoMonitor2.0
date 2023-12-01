7dd2ed239b2520e84ce56f3c66220eaedfa66085
Em1rio <48679388+Em1rio@users.noreply.github.com>
2023-12-01 19:50:52 +0300

    Создал форматтер

---
+++ CryptoMonitorMVVM/Extensions/Results.swift
@@ -0,0 +1,8 @@
+//
+//  Results.swift
+//  CryptoMonitorMVVM
+//
+//  Created by Emir Nasyrov on 01.12.2023.
+//
+
+import Foundation
---
+++ CryptoMonitorMVVM/Services/Formatter.swift
@@ -0,0 +1,40 @@
+//
+//  Formatter.swift
+//  CryptoMonitorMVVM
+//
+//  Created by Emir Nasyrov on 01.12.2023.
+//
+
+import Foundation
+
+final class Formatter {
+
+    static let shared = Formatter()
+
+    private init() { }
+
+    func format(inputValue: String) -> String {
+
+        let value = Decimal(string: inputValue )
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumFractionDigits = 16
+        return numberFormatter.string(for: value)!
+    }
+    func formatPercentAndAverage(inputValue: String) -> String {
+
+        let value = Decimal(string: inputValue )
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumFractionDigits = 2
+        return numberFormatter.string(for: value)!
+    }
+    func formatCurrency(inputValue: String) -> String {
+        let value = Decimal(string: inputValue)
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.maximumFractionDigits = 2
+        numberFormatter.locale = Locale(identifier: "es_CL")
+        return numberFormatter.string(for: value)!
+    }
+}
