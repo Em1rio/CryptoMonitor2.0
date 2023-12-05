d169ef79f0a66f3cf58e9347ec47628e684a5a25
Em1rio <48679388+Em1rio@users.noreply.github.com>
2023-12-05 17:47:38 +0300

    Начал создавать и настраивать карточку актива

--- CryptoMonitorMVVM/Views/ViewControllers/DetailViewController.swift
+++ CryptoMonitorMVVM/Views/ViewControllers/DetailViewController.swift
@@ -14,23 +14,36 @@ final class DetailViewController: UIViewController {
     
     // MARK: - UI Components
     //TODO:
-    //Большая вью с закругленными краями
-    //Внутри нее информация о выбранной монете:
-    //Рыночная цена отформатированная с $
-    //Изменение за 24 часа в %
-    //Средняя стоимость активов с $
-    //Размер актива(количество)
-    //Стоимость на данный момнет в $
-    //Изменение за все время в %
-    //Сделать таблицу
+    // Разместить все лейблы во вьюхах
+    // Настроить лейблы в таблице
+
     // MARK: - UI Components
     private lazy var displayView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .systemMint
-        view.backgroundColor?.withAlphaComponent(0.1)
-        view.layer.cornerRadius = 16
-        return view
+        return createCustomView(withBackgroundColor: .systemMint, cornerRadius: 16)
     }()
+    private lazy var detailsView: UIView = {
+        return createCustomView(withBackgroundColor: .systemRed)
+    }()
+
+    private lazy var numberOfCoinsLabel: UILabel = {
+        return createLabel(withText: "0.00000000 BTC", fontSize: 30)
+    }()
+    private lazy var coinsValueLabel: UILabel = {
+        return createLabel(withText: "= 100000 $", fontSize: 20)
+    }()
+    private lazy var detailsLabel: UILabel = {
+        return createLabel(withText: "Показатели", fontSize: 20)
+    }()
+    private lazy var averagePriceLabel: UILabel = {
+        return createLabel(withText: "$ 25000,0", fontSize: 15)
+    }()
+    private lazy var changePerDayLabel: UILabel = {
+        return createLabel(withText: "+ 100 %", fontSize: 15)
+    }()
+    private lazy var changeOverTimeLabel: UILabel = {
+        return createLabel(withText: "+ 1000 %", fontSize: 15)
+    }()
+    
     private(set) lazy var tableView: UITableView = {
         let tableView = UITableView()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
@@ -58,12 +71,51 @@ final class DetailViewController: UIViewController {
     // MARK: - UI Setup
     private func setupUI() {
         view.backgroundColor = .systemBackground
-        setupDisplayView()
+        setupViews()
+        setupLabels()
         setupTableView()
     }
-    private func setupDisplayView() {
+    private func setupViews() {
+        let averagePriceView = createCustomView(withBackgroundColor: .systemBackground)
+        let changeOverTimeView = createCustomView(withBackgroundColor: .systemBackground)
+        let changePerDayView = createCustomView(withBackgroundColor: .systemBackground)
+        
         self.view.addSubview(displayView)
+        self.displayView.addSubview(detailsView)
+        self.detailsView.addSubview(averagePriceView)
+        self.detailsView.addSubview(changeOverTimeView)
+        self.detailsView.addSubview(changePerDayView)
+        
         self.displayView.translatesAutoresizingMaskIntoConstraints = false
+        self.detailsView.translatesAutoresizingMaskIntoConstraints = false
+        averagePriceView.translatesAutoresizingMaskIntoConstraints = false
+        changeOverTimeView.translatesAutoresizingMaskIntoConstraints = false
+        changePerDayView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Создание StackView для размещения view в сетке 2 на 2
+            let stackView = UIStackView()
+            stackView.axis = .vertical // Ось вертикальная для создания 2 ряда
+            stackView.distribution = .fillEqually // Равномерное заполнение по вертикали
+            stackView.spacing = 8 // Отступ между view
+
+            // Создание верхнего ряда StackView
+            let topRowStackView = UIStackView()
+            topRowStackView.axis = .horizontal // Ось горизонтальная для создания 2 столбцов
+            topRowStackView.distribution = .fillEqually // Равномерное заполнение по горизонтали
+            topRowStackView.spacing = 8 // Отступ между view в ряду
+
+            // Добавление view в верхний ряд StackView
+            topRowStackView.addArrangedSubview(averagePriceView)
+            topRowStackView.addArrangedSubview(changeOverTimeView)
+            // Добавление верхнего ряда в главный StackView
+            stackView.addArrangedSubview(changePerDayView)
+            
+            // Добавление последнего view в нижний ряд StackView
+            stackView.addArrangedSubview(topRowStackView)
+
+            // Добавление StackView на detailsView
+            detailsView.addSubview(stackView)
+            stackView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             self.displayView.topAnchor.constraint(
@@ -73,9 +125,56 @@ final class DetailViewController: UIViewController {
             self.displayView.trailingAnchor.constraint(
                 equalTo: self.view.trailingAnchor, constant: -10),
             self.displayView.heightAnchor.constraint(
-                equalTo: self.view.heightAnchor, multiplier: 0.3)
+                equalTo: self.view.heightAnchor, multiplier: 0.3),
+            
+            self.detailsView.centerXAnchor.constraint(equalTo: self.displayView.centerXAnchor),
+            self.detailsView.bottomAnchor.constraint(equalTo: self.displayView.bottomAnchor, constant: -10),
+            self.detailsView.leadingAnchor.constraint(equalTo: self.displayView.leadingAnchor, constant: 10),
+            self.detailsView.trailingAnchor.constraint(equalTo: self.displayView.trailingAnchor, constant: -10),
+            self.detailsView.heightAnchor.constraint(equalTo: self.displayView.heightAnchor, multiplier: 0.55),
+            
+            stackView.topAnchor.constraint(equalTo: detailsView.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: detailsView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: detailsView.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: detailsView.bottomAnchor)
+                
         ])
     
+    }
+    private func setupLabels() {
+        self.displayView.addSubview(numberOfCoinsLabel)
+        
+        self.displayView.addSubview(coinsValueLabel)
+        
+        //self.detailsView.addSubview(detailsLabel)
+//        self.detailsView.addSubview(averagePriceLabel)
+//        self.detailsView.addSubview(changePerDayLabel)
+//        self.detailsView.addSubview(changeOverTimeLabel)
+        
+        self.numberOfCoinsLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.coinsValueLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.detailsLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.averagePriceLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.changePerDayLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.changeOverTimeLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.numberOfCoinsLabel.centerXAnchor.constraint(equalTo: self.displayView.centerXAnchor),
+            self.numberOfCoinsLabel.topAnchor.constraint(equalTo: self.displayView.topAnchor, constant: 10),
+            
+            self.coinsValueLabel.topAnchor.constraint(equalTo: self.numberOfCoinsLabel.bottomAnchor, constant: 5),
+            self.coinsValueLabel.centerXAnchor.constraint(equalTo: self.displayView.centerXAnchor),
+            
+
+//            self.detailsLabel.topAnchor.constraint(equalTo: self.detailsView.topAnchor, constant: 5),
+//            self.detailsLabel.leadingAnchor.constraint(equalTo: self.detailsView.leadingAnchor, constant: 10),
+
+            
+        ])
+        
+        
+        
     }
     
     private func setupTableView() {
@@ -96,5 +195,20 @@ final class DetailViewController: UIViewController {
         ])
     }
     
+    private func createLabel(withText text: String, fontSize: CGFloat) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.font = UIFont.systemFont(ofSize: fontSize)
+        label.textAlignment = .center
+        return label
+        
+    }
+    private func createCustomView(withBackgroundColor backgroundColor: UIColor, cornerRadius: CGFloat = 0) -> UIView {
+        let customView = UIView()
+        customView.backgroundColor = backgroundColor
+        customView.layer.cornerRadius = cornerRadius
+        customView.layer.masksToBounds = cornerRadius > 0
+        return customView
+    }
 
 }
