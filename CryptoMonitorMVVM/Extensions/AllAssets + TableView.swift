190f5fc774d9933ee0cd351566f9a45af39901a1
Em1rio <48679388+Em1rio@users.noreply.github.com>
2023-12-13 17:20:42 +0300

    Добавил disclosureIndicator, добавил количество монет, задал размер ячейки передал координатору название монеты, для отображения в тайтле след экрана

--- CryptoMonitorMVVM/Extensions/AllAssets + TableView.swift
+++ CryptoMonitorMVVM/Extensions/AllAssets + TableView.swift
@@ -14,10 +14,17 @@ extension AllAssetsViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.accessoryType = .disclosureIndicator
         var content = cell.defaultContentConfiguration()
         if let coins = viewModel.coinsAsCategory, !coins.isEmpty  {
             let coin = coins[indexPath.row]
             content.text = coin.nameCoin
+            if let coinQuantity = coin.coinQuantity {
+                let formattedQuantity = Formatter.shared.format("\(coinQuantity)")
+                content.secondaryText = "Общее количество: \(formattedQuantity)"
+            } else {
+                content.secondaryText = "Общее количество: N/A"
+            }
         } else {
             
         }
@@ -25,10 +32,17 @@ extension AllAssetsViewController: UITableViewDataSource, UITableViewDelegate {
         cell.contentConfiguration = content
         return cell
     }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
     //TODO: Обработка нажатия на ячейку
     // через замыкание передать данные из детейлVC
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        coordinator?.coordinateToDetail()
+        if let coins = viewModel.coinsAsCategory, !coins.isEmpty {
+            let pressedCoin = coins[indexPath.row]
+            coordinator?.coordinateToDetail(_with: pressedCoin.nameCoin)
+        }
+        
     }
     
 }
