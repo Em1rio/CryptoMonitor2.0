810306ea541407b3176fd41d6bc5687f62078766
Em1rio <48679388+Em1rio@users.noreply.github.com>
2024-02-20 17:13:44 +0300

    Добавил отображение галочки при успешном добавлении записи в базу

--- CryptoMonitorMVVM/Extensions/Main+CollectionView.swift
+++ CryptoMonitorMVVM/Extensions/Main+CollectionView.swift
@@ -15,7 +15,6 @@ extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSour
         }else {
             return self.viewModel.quickAccessCoinsCells.count + 1
         }
-        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -48,15 +47,18 @@ extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSour
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-      
+        
         return CGSize(width: (numPadContainerSize.width/3)-2, height: (numPadContainerSize.height/4)-2)
     }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 2
     }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 2
     }
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if collectionView == numPadCollectionView {
             // Обработка выбора элемента в numPadCollectionView
@@ -69,43 +71,38 @@ extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSour
             } else {
                 observableToUpdate = priceLabelObservable
             }
-            self.viewModel.numPadButtonTapped(buttonCell,
-                                              forSegmentIndex: selectedSegmentIndex,
-                                              observableToUpdate: &observableToUpdate)
+            self.viewModel.numPadButtonTapped(
+                buttonCell,forSegmentIndex: selectedSegmentIndex, observableToUpdate: &observableToUpdate)
             let cell = collectionView.cellForItem(at: indexPath) as? NumberPadCell
             let originalColor = cell?.backgroundColor
             cell?.backgroundColor = .secondarySystemBackground
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                 cell?.backgroundColor = originalColor
             }
-            
-
         } else if collectionView == coinCollectionView {
             // Обработка выбора элемента в coinCollectionView
             if indexPath.row == 0 {
                 coordinator?.coordinateToAllCoins { [weak self] AllCoinsCellModel in
                     guard let quantity = self?.quantityLabelObservable.value,
                           let price = self?.priceLabelObservable.value else {
-                            print("Somethings goes wrong")
+                        print("Somethings goes wrong")
                         self?.generator.notificationOccurred(.error)
                         return
                     }
                     self?.viewModel.saveToDataBase(AllCoinsCellModel, isPurchase: self?.isPurchase ?? true, quantity: Decimal128(value: quantity), price: Decimal128(value: price))
                     self?.setLabelsOnDefaultState()
                     self?.generator.notificationOccurred(.success)
-                    //TODO: ActivityIndicator с галочкой
+                    self?.showCheckmark()
                 }
             } else {
                 let buttonCell = self.viewModel.quickAccessCoinsCells[indexPath.row - 1]
                 self.viewModel.saveToDataBase(buttonCell, isPurchase: self.isPurchase, quantity: Decimal128(value: quantityLabelObservable.value ?? 0.0), price: Decimal128(value: priceLabelObservable.value ?? 0.0))
                 self.setLabelsOnDefaultState()
-                 //Тактильная отдача при нажатии
+                //Тактильная отдача при нажатии
                 self.generator.notificationOccurred(.success)
-                //TODO: ActivityIndicator с галочкой
-                
+                showCheckmark()
             }
         }
     }
- 
-   
+    
 }
