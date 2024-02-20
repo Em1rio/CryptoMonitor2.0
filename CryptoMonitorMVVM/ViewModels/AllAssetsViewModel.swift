358fe349de7e5b1ea73352ac878517c04266311b
Em1rio <48679388+Em1rio@users.noreply.github.com>
2024-02-20 17:54:28 +0300

    Изменил то когда обновляется лейбл с общей суммой

--- CryptoMonitorMVVM/ViewModels/AllAssetsViewModel.swift
+++ CryptoMonitorMVVM/ViewModels/AllAssetsViewModel.swift
@@ -8,8 +8,6 @@
 import Foundation
 import RealmSwift
 
-//TODO: 
-//Без интернета показывается стоимость которую расчитал пока интернет был, нужно показывать уведобление что цена не актуальная, подключите интернет
 final class AllAssetsViewModel {
     // MARK: - Variables
     private let networkManager: NetworkManagerProtocol
@@ -18,79 +16,96 @@ final class AllAssetsViewModel {
     private(set) var coinsAsCategory: Results<CoinCategory>?
     var callTableView: (()-> Void)?
     var callBalanceLabel: ((_ totalCost: String) -> Void)?
-
+    
     // MARK: - Lifecycle
     init(_ networkManager: NetworkManagerProtocol,
          _ dataBaseManager: DBManagerProtocol) {
         self.networkManager = networkManager
         self.dataBaseManager = dataBaseManager
     }
+    
     //MARK: - Data source for TableView
     func numberOfRow(_ section: Int) -> Int {
         if let coins = coinsAsCategory, !coins.isEmpty  {
             return coins.count
         } else { return 0}
     }
+    
     func loadDataFromDatabase()  {
         coinsAsCategory = dataBaseManager.getObject(ofType: CoinCategory.self)
         callTableView?()
     }
-
+    
     //MARK: - Update All Assets Cost Label
     func fetchDataFromDB() {
         totalCost = 0.0
         guard let coins = coinsAsCategory?.toArray(ofType: CoinCategory.self) as? [CoinCategory] else {return}
+        var compleatedIterations = 0
         for item in coins {
-            fetchData(for: item)
+            fetchData(for: item) {
+                compleatedIterations += 1
+                if compleatedIterations == coins.count {
+                    DispatchQueue.main.async { [weak self] in
+                        guard let self = self else { return }
+                        let formattedTotalCost = Formatter.shared.format("\(self.totalCost)", format: .currencyShort)
+                        self.updateUI(with: formattedTotalCost)
+                    }
+                }
+            }
+            
         }
     }
     
-   private func fetchData(for item: CoinCategory) {
+    
+    
+    private func fetchData(for item: CoinCategory, completion: @escaping () -> Void) {
         let idNumber = item._id
         networkManager.fetchTickerDetails(withID: idNumber) { result in
-        switch result {
-                case .success(let coinData):
-                    do {
-                        let coin = try JSONDecoder().decode([OneCoin].self, from: coinData)
-                        self.coinsData(coin, for: item)
-                    } catch {
-                        print("Failed to decode coin data with error: \(error)")
-                    }
-                case .failure(let error):
-                    print("Network request failed with error: \(error)")
+            switch result {
+            case .success(let coinData):
+                do {
+                    let coin = try JSONDecoder().decode([OneCoin].self, from: coinData)
+                    self.coinsData(coin, for: item)
+                } catch {
+                    print("Failed to decode coin data with error: \(error)")
                 }
+            case .failure(let error):
+                print("Network request failed with error: \(error)")
             }
+            completion()
+        }
     }
     
-   private func coinsData(_ coin: [OneCoin] , for item: CoinCategory) {
-       let priceRightNow = try! Decimal128(string: coin.first?.priceUsd ?? "0")
-       DispatchQueue.main.async { [weak self] in
-        guard let self = self, let coinID = coin.first?.id else { return }
-           let realmQuery = self.dataBaseManager.getRealmQuery(
-               forType: CoinCategory.self,
-               where: "_id",
-               equals: coinID)
-           
-                if let quantity = realmQuery.first?.coinQuantity {
-                   let rawTotalCost = self.calculateTotalCost(priceRightNow, quantity)
-                   self.totalCost += rawTotalCost
-                    let formattedTotalCost = Formatter.shared.formatCurrencyShort("\(self.totalCost)")
-                   self.updateUI(with: formattedTotalCost)
-               } else {
-                   return
-               }
-           }
-        
+    
+    
+    
+    private func coinsData(_ coin: [OneCoin] , for item: CoinCategory) {
+        let priceRightNow = try! Decimal128(string: coin.first?.priceUsd ?? "0")
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self, let coinID = coin.first?.id else { return }
+            let realmQuery = self.dataBaseManager.getRealmQuery(
+                forType: CoinCategory.self,
+                where: "_id",
+                equals: coinID)
+            
+            if let quantity = realmQuery.first?.coinQuantity {
+                let rawTotalCost = self.calculateTotalCost(priceRightNow, quantity)
+                self.totalCost += rawTotalCost
+            } else {
+                return
+            }
+        }
     }
+    
     private func calculateTotalCost(_ price: Decimal128, _ quantity: Decimal128) -> Decimal128 {
         return price * quantity
     }
+    
     private func updateUI(with totalCost: String) {
-            self.callBalanceLabel?(totalCost)
+        self.callBalanceLabel?(totalCost)
     }
 }
 //MARK: - TODO: Список задач
-// Обновление UI делать не в цикле
 // Посчитать разницу в стоимости портфеля за все время
 // Сделать возможность отсортировать в алфавитном порядке или по стоимости активов
 
